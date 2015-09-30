18ca250ba2c84c9fdbbd7caebf06e8b7ea9537c9
Volodymyr Roman <vova.rova@gmail.com>
2015-10-01 00:43:01 +0300

    add cli

--- src/main/java/fine/project/App.java
+++ src/main/java/fine/project/App.java
@@ -1,19 +1,48 @@
 package fine.project;
 
 import java.util.Map;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
 /**
  * Main class
+ * 
  * @author Vroman
  *
  */
 public class App {
+
+	private static Options options = new Options();
+	static {
+		options.addOption("tw", true, "Word template location");
+		options.addOption("ex", true, "Excel location");
+		options.addOption("rw", true, "Result Word location");
+		options.addOption("h", false, "help");
+	}
+	private static HelpFormatter helpFormatter = new HelpFormatter();
+	private static CommandLineParser parser = new DefaultParser();
+	
 	public static void main(String[] args) throws Exception {
+		CommandLine cmd = parser.parse(options, args);
 		
-		Map<String, String> readKeys = XmlReader.readKeys("D:/lab2.xlsx");
-		WordReplacer wordReplacer = new WordReplacer("D:/w.docx");
+		if(args.length==0 || cmd.hasOption("h")){
+			helpFormatter.printHelp("Fine Generator", options);
+			return;
+		}
+		String templateLocation = cmd.getOptionValue("tw");
+		String excellLocation = cmd.getOptionValue("ex");
+		String resultLocation = cmd.getOptionValue("rw");
+		
+		
+		Map<String, String> readKeys = XmlReader.readKeys(excellLocation);
+		WordReplacer wordReplacer = new WordReplacer(templateLocation);
 		wordReplacer.readDocument();
 		wordReplacer.replaceText(readKeys);
-		wordReplacer.saveWord("D:/wq.docx");
-		
+		wordReplacer.saveWord(resultLocation);
 	}
+
 }
